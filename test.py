ec0da2cbd6c2b67a6763440e0c40f2e31853a712
naufalpry <90716951+naufalpry@users.noreply.github.com>
2023-08-16 23:39:29 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,43 +1,5 @@
-def generateSquare(n):
+Python puzzle magic square
 
-	puzzleSquareNumber = [[0 for x in range(n)]
-				for y in range(n)]
-
-	i = n // 2
-	j = n - 1
-
-	num = 1
-	while num <= (n * n):
-		if i == -1 and j == n:
-			j = n - 2
-			i = 0
-		else:
-			if j == n:
-				j = 0
-			if i < 0:
-				i = n - 1
-
-		if puzzleSquareNumber[int(i)][int(j)]: 
-			j = j - 2
-			i = i + 1
-			continue
-		else:
-			puzzleSquareNumber[int(i)][int(j)] = num
-			num = num + 1
-
-		j = j + 1
-		i = i - 1 
-
-	print("Puzzle square number for n =", n)
-	print("Sum of each row or column",
-		n * (n * n + 1) // 2, "\n")
-
-	for i in range(0, n):
-		for j in range(0, n):
-			print('%2d ' % (puzzleSquareNumber[i][j]),
-			end='')
-			if j == n - 1:
-				print()
-
-n = 3
-generateSquare(n)
+`Instalation python`
+How to running : 
+`Python puzzle.py`
